461eac287b6dd310c663fec523b916953b1fe4fc
olawale <olaclassik@gmail.com>
2018-12-15 12:31:41 +0100

    Refactor templates locations in threads/tests

--- threads/tests.py
+++ threads/tests.py
@@ -5,7 +5,7 @@ class ThreadsPageTest(TestCase):
 
     def test_uses_threads_list_template(self):
         response = self.client.get('/threads/list')
-        self.assertTemplateUsed(response, 'list.html')
+        self.assertTemplateUsed(response, 'threads/list.html')
 
 
 
