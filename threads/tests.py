ee69b676e30c819001d58ffd4c895add0ab03b67
olawale <olaclassik@gmail.com>
2018-12-16 07:01:22 +0100

    Redirect after POST, and show all items in template

--- threads/tests.py
+++ threads/tests.py
@@ -12,13 +12,39 @@ class ThreadsPageTest(TestCase):
             'title_text': 'Boys in Afesiere sch',
             'content_text': 'Afesiere boys are best in the entire Ughelli North among all secondary school boys',
             })
-        self.assertIn('Boys in Afesiere sch', response.content.decode())
-        self.assertTemplateUsed(response, 'threads/list.html')
 
+        self.assertEqual(Content.objects.count(), 1)
+        new_content = Content.objects.first()
+        self.assertEqual(new_content.title, 'Boys in Afesiere sch')
+
+    def test_can_redirects_after_POST(self):
+        response = self.client.post('/threads/list', data={
+            'title_text': 'Boys in Afesiere sch',
+            'content_text': 'Afesiere boys are best in the entire Ughelli North among all secondary school boys',
+            })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/threads/list')
+
+    def test_only_saves_contents_when_necessary(self):
+        self.client.get('/threads/list')
+        self.assertEqual(Content.objects.count(), 0)
+
+    def test_displays_all_content_items(self):
+        Content.objects.create(title='Contenty 1', body="body of content")
+        Content.objects.create(title='Contenty 2', body="body of content")
+
+        response = self.client.get('/threads/list')
+
+        self.assertIn('Contenty 1', response.content.decode())
+        self.assertIn('Contenty 2', response.content.decode())
+        
 
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_content(self):
+        # TODO: pay attention to this space, there
+        # TODO cont'd: is a more compact way to write this unittest
+
         first_content = Content()
         first_content.title = 'The first (ever) list content'
         first_content.save()
--- threads/views.py
+++ threads/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from threads.models import Content
 
 
 def threads_page(request):
-    title = request.POST.get('title_text', '')
-    content = request.POST.get('content_text', '')
-    context = {
-        'new_content_title': title,
-    }
+    # TODO: Display multiple items in the table
+    # TODO: Support more than one list!
+    if request.method == 'POST':
+        new_title = request.POST['title_text']
+        new_body = request.POST['content_text']
+        Content.objects.create(title=new_title, body=new_body)
+        return redirect('/threads/list')
+
+    contents = Content.objects.all()
+    context = {'contents': contents}
     return render(request, 'threads/list.html', context)
