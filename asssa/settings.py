bfab6e624de51b31b88dc67236d3a70c35076b26
olawale <olaclassik@gmail.com>
2019-02-11 22:37:35 +0100

    installations and some cleanups

--- asssa/settings.py
+++ asssa/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # My Apps
-    'threads',
+
+    # Third party lib
+  
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
+}
--- asssa/urls.py
+++ asssa/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('threads/', include('threads.urls', namespace="threads")),
+
 ]
