e83f6ad973d123f3311abea2ec444c4cd395e4a3
olawale <olaclassik@gmail.com>
2019-03-06 15:53:26 +0100

    adjusted comment serializer

--- comment/serializers.py
+++ comment/serializers.py
@@ -6,7 +6,27 @@ from .models import Comment
 class CommentListSerializer(ModelSerializer):
 	class Meta:
 		model = Comment
-		fields = '__all__'
+		fields = [
+			"id",
+			"user",
+			"parent",
+			"body",
+			"level",
+			"comment_on",
+			"created_on",
+		]
 
 
+"""
+	"id": 1,
+    "body": "Hello There",
+    "created_on": "2019-03-06T14:26:15.610849Z",
+    "lft": 1,
+    "rght": 4,
+    "tree_id": 1,
+    "level": 0,
+    "comment_on": 3,
+    "parent": null,
+    "user": 13
 
+"""
