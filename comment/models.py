f5ed5c57f7bec66571b08c6bf5a324b2b5363bcb
olawale <olaclassik@gmail.com>
2019-03-07 17:27:23 +0100

    convert indentation to spaces

--- comment/models.py
+++ comment/models.py
@@ -3,51 +3,44 @@ from threads.models import Thread
 from django.conf import settings
 from mptt.models import MPTTModel, TreeForeignKey
 import uuid as uuid_lib
-from shortuuidfield import ShortUUIDField
+# from shortuuidfield import ShortUUIDField
 import shortuuid
 
 User = settings.AUTH_USER_MODEL
 
 
 class CommentManager(models.Manager):
-	def all(self):
-		qs = super(CommentManager, self).filter(parent=None)
-		return qs
+    def all(self):
+        qs = super(CommentManager, self).filter(parent=None)
+        return qs
 
 
 class Comment(MPTTModel):
-	
-	uuid = models.UUIDField(db_index=True,
+
+    uuid = models.UUIDField(db_index=True,
                             default=uuid_lib.uuid4,
                             editable=False)
-	body = models.CharField(max_length=280)
-	comment_on = models.ForeignKey(Thread, on_delete=models.CASCADE, null=True, related_name='comments')
-	parent = TreeForeignKey('self', null=True, blank=True, related_name='replies', db_index=True, on_delete=models.CASCADE)
-	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
-	created_on = models.DateTimeField(auto_now_add=True, editable=False)
+    body = models.CharField(max_length=280)
+    comment_on = models.ForeignKey(Thread, on_delete=models.CASCADE, null=True, related_name='comments')
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='replies', db_index=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    created_on = models.DateTimeField(auto_now_add=True, editable=False)
 
-	# url = ShortUUIDField(db_index=True, default=uuid_lib.uuid4, editable=False, unique=True)
+    # url = ShortUUIDField(db_index=True, default=uuid_lib.uuid4, editable=False, unique=True)
 
-	objects = CommentManager()
+    objects = CommentManager()
 
-	def __str__(self):
-		return str(f'{self.user}: {self.body[:50]}...')
+    class MPTTMeta:
+        order_insertion_by = ['name']
 
-	def children(self):
-		return Comment.objects.filter(parent=self)
+    def __str__(self):
+        return str(f'{self.user}: {self.body[:50]}...')
 
-	@property
-	def is_parent(self):
-		if self.parent is not None:
-			return False
-		return True
-	
+    def children(self):
+        return Comment.objects.filter(parent=self)
 
-
-
-
-
-
-
-
-
+    @property
+    def is_parent(self):
+        if self.parent is not None:
+            return False
+        return True
