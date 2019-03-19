634d5c155800b1b1c2aac45887d7c8d1bbae460f
olawale <olaclassik@gmail.com>
2019-03-19 19:39:07 +0100

    modified settings

--- asssa/settings.py
+++ asssa/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from datetime import timedelta
-import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -168,4 +168,5 @@ SIMPLE_JWT = {
 
 
 # Activate Django-Heroku.
+import django_heroku
 django_heroku.settings(locals())
