3148a174c97978cd842e88a3cbaf8604b8359f80
olawale <olaclassik@gmail.com>
2019-03-31 19:46:42 +0100

    complete design of login and footer

--- src/components/authenticate/Login.js
+++ src/components/authenticate/Login.js
@@ -1,12 +1,107 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import { TextField, Paper, Typography, Button } from "@material-ui/core";
+
+const styles = (theme) => ({
+  container: {
+    margin: "1rem auto"
+  },
+  title: {
+    textAlign: "center"
+  },
+  form: {
+    margin: "0 1rem"
+  },
+  button: {
+    margin: "1rem auto"
+  }
+});
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: "",
+      role: "1",
+      errors: {}
+    };
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  handleChange = (name) => (event) => {
+    this.setState({
+      [name]: event.target.value
+    });
+  };
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const loginUser = {
+      username: this.state.username,
+      role: this.state.role,
+      password: this.state.password
+    };
+    console.log(loginUser);
+  }
+
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <h1>Login Now</h1>
-      </div>
-    )
+      <Paper className={classes.container} elevation={0}>
+        <div className={classes.title}>
+          <Typography variant="h6" component="h2">
+            Login
+          </Typography>
+
+          <Typography variant="subtitle1" component="h3" color="textSecondary">
+            Login into your ASSA forum account.
+          </Typography>
+        </div>
+        <form className={classes.form} noValidate autoComplete="off">
+          <TextField
+            required
+            id="username-input"
+            label="Enter Username or Email"
+            type="text"
+            name="text"
+            margin="dense"
+            value={this.state.username}
+            onChange={this.handleChange("username")}
+            variant="outlined"
+            fullWidth
+          />
+
+          <TextField
+            required
+            id="password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            margin="dense"
+            variant="outlined"
+            value={this.state.password}
+            onChange={this.handleChange("password")}
+            fullWidth
+          />
+
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            onClick={this.onSubmit}
+          >
+            Log In
+          </Button>
+        </form>
+      </Paper>
+    );
   }
 }
-export default Login;
+
+Login.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+export default withStyles(styles)(Login);
--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -1,9 +1,33 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import { Paper, Typography } from "@material-ui/core";
 
-export default function() {
+const styles = (theme) => ({
+  container: {
+    backgroundColor: "#000",
+    textAlign: "center"
+  },
+  footer: {
+    color: "#eee"
+  }
+});
+
+function Footer(props) {
+  const { classes } = props;
   return (
-    <div>
-      <footer>Copyright &copy; {new Date().getFullYear()} Olawale</footer>
-    </div>
+    <Paper elevation={0} square={true} className={classes.container}>
+      <footer>
+        <Typography variant="body2" component="h6" className={classes.footer}>
+          Copyright &copy; {new Date().getFullYear()} Olawale
+        </Typography>
+      </footer>
+    </Paper>
   );
 }
+
+Footer.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Footer);
