47dc7a99e875c9e09415ec29f3df1c1aedc57d12
olawale <olaclassik@gmail.com>
2019-06-04 13:41:21 +0100

    done with the home page

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,3 @@
-// const cy = require("cypress");
-
 describe("CSS-Tricks home page", function() {
   beforeEach(function() {
     cy.visit("https://css-tricks.com/");
@@ -11,5 +9,36 @@ describe("CSS-Tricks home page", function() {
 
   it("has a visible star logo", function() {
     cy.get(".icon-logo-star").should("be.visible");
+  });
+
+  describe("with a 320x568 viewport", function() {
+    beforeEach(function() {
+      cy.viewport(320, 568);
+    });
+
+    it("has a visible mobile menu toggle", function() {
+      cy.get("#mobile-nav-button").should("be.visible");
+    });
+  });
+
+  describe("with a 1234 viewport", function() {
+    beforeEach(function() {
+      cy.viewport(800, 660);
+    });
+
+    it("has a visible mobile menu toggle", function() {
+      cy.get("#mobile-nav-button").should("be.visible");
+    });
+  });
+
+  describe("site search", function() {
+    beforeEach(function() {
+      cy.viewport(1285, 660);
+      cy.get(".search-field").type("flexbox{enter}");
+    });
+
+    it("requests the results", function() {
+      cy.url().should("include", "?s=flexbox");
+    });
   });
 });
